8d5c50e8af8df06aa77c7343d0ae10ad27425d36
taeyoungro <tyro981009@naver.com>
2025-12-07 20:22:20 +0900

    Update main.tf

--- AWS-TF/environments/prod/main.tf
+++ AWS-TF/environments/prod/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "nty-terraform-state-prod"
+    bucket         = "nty-terraform-state-test"
     key            = "terraform/terraform.tfstate" # 수정됨: S3 내 파일 저장 경로
     region         = "us-west-2"                   # 수정됨: 시나리오에 맞춰 버지니아로 변경 (버킷도 이 리전에 있어야 함)
     encrypt        = true
-    dynamodb_table = "nty-terraform-lock-prod"
+    dynamodb_table = "nty-terraform-lock-test"
     profile        = "developer" # 추가됨: Step 3에서 등록한 프로필 사용
   }
 }
@@ -23,5 +23,5 @@ provider "aws" {
 
 module "network" {
   source   = "../../modules/network"
-  env_name = "prod" # 수정됨: prod로 변경123456
+  env_name = "test" # 수정됨: prod로 변경123456
 }
