8d62526c9cdfdf940c5618e1654bd369b390d4b7
taeyoungro <tyro981009@naver.com>
2025-12-07 20:17:21 +0900

    Update providers.tf

--- AWS-TF/environments/prod/providers.tf
+++ AWS-TF/environments/prod/providers.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-# Provider 설정: 서울 리전
-provider "aws" {
-  region = "us-east-2"
-}
+
